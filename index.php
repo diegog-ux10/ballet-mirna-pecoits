0c41cb4e76e9cbe2fc004d3ae1154599943d9dd9
Diego Granados <diego@codeando.edu.pe>
2022-04-25 12:30:09 -0400

    Inicio de Proyecto

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<body>
+    <main>
+        <header class="main-container align header">
+            <div class="secondary-container">
+                <img src="<?php echo get_theme_file_uri() . "/assets/img/logo.png" ?>" alt="Logo de la Empresa">
+                <img src="<?php echo get_theme_file_uri() . "/assets/img/Logo-45-anos.png" ?>" alt="Logo de 45 años">
+                <nav>
+                    <ul>
+                        <li><a href="">Ballet Mirna Pécoits</a></li>
+                        <li><a href=""></a>Linha do Tempo</li>
+                        <li><a href="">Cia. de Danca</a></li>
+                        <li><a href=""></a>Contato</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </main>
+    
+
+<footer>
+    <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+    <div class="wrapper-content">
+        <div class="title">
+            <h4>Resultados da pesquisa para: <?php echo $_GET['s']; ?></h4>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
