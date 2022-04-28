173411b608450fc8726a0573deaa4d918c3eabde
Diego Granados <diego@codeando.edu.pe>
2022-04-28 13:00:24 -0400

    Layout for Fron Page
    Created footer, and all the sections needed for front page. included his styles

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="main-container">
+    <?php wp_footer(); ?>
+    <div class="secondary-container align footer">
+        <span>
+            <p class="text-three">Políticas de Privacidade</p>
+        </span><span>
+            <p class="text-three">Copyright © 2022 Ballet Mirna Pécoits - Todos os Direitos Reservados</p>
+        </span><span>
+            <p class="text-three">Tecnologia: </p>
+            <img src="<?php echo get_theme_file_uri() . '/assets/img/logo-ceicom-blanco.png' ?>" alt="logo-png">
+        </span>
+    </div>
+
+</footer>
--- front-page.php
+++ front-page.php
@@ -16,10 +16,42 @@
                 </p>
             </div>
         </div>
-    </main>  
+    </main>
+    <section class="main-container ballet" style="background-image: url('<?php echo get_theme_file_uri() . '/assets/img/fondo2.png'; ?>');
+    background-position: center center; background-size: cover">
+        <div class="secondary-container columns ballet-container vertical-padding">
+           
+                <h2 class="title-two">Ballet Mirna Pecóits</h2>
+                <p class="text-two tex-width-one margin-bottom-one">Ballet Mirna Pécoits. Fazendo arte com qualidade.<br>
+                Indicada para qualquer idade, a dança contribui para um bom desenvolvimento especialmente
+                de crianças e adolescentes. A dança molda o corpo, melhora a postura e corrige problemas físicos,
+                ajudando a desenvolver a concentração e memória,  discilplina e o auto controle, facilita a expressão,
+                a criatividade a coordenação e melhora a auto-estima.</p>
+                <h3 class="title-three text-width-two margin-bottom-one">Muitos benefícios para a mente e o corpo.</h3>
+                <h2 class="title-two">Aulas</h2>
+                <h4 class="title-four text-width-two margin-bottom-one">Ballet Clássico | Baby Class | Danças Urbanas Hip Hop Kids | Jazz e Sapateado</h4>
+                <p class="text-two text-width-two">Escolha a aula que se encaixa mais com o seu perfil,
+                e venha dançar com a gente!</p>
+                
 
-    <footer>
-        <?php wp_footer(); ?>
-    </footer>
+        </div>
+    </section>
+    
+    <section class="main-container cia">
+        <div class="secondary-container cia-container vertical-padding columns">
+            <img src="<?php echo get_theme_file_uri() . '/assets/img/fotos.png' ?>" alt="
+            fotos de balé" class="fotos">
+            <h2 class="title-five margin-bottom-one">Cia de Dança</h2>
+            <p class="text text-width">Incentivada por bailarinos, pessoas ligadas à cultura e amantes da dança, foi fundada em 03 de março de 2010 a Companhia da Dança Mirna Pécoits.<br><br>
+
+            Nosso objetivo é promover, produzir e realizar espetáculos a nível amador e profissional, desenvolver a dança e atividades culturais na cidade de Francisco Beltrão – PR e região, realizar cursos, promover debates, participar de festivais e desenvolver intercâmbio com outros grupos ou atividades culturais.<br><br>
+
+            A Cia também busca promover debates, cursos, participar de festivais e ainda desenvolver intercâmbio de conhecimento entre os estudantes e profissionais da dança.</p>
+        </div>
+    </section>
+
+    
+    <?php get_footer(); ?>
+    
 </body>
 </html>
